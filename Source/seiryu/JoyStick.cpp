1fb64794d01af88c698d7bd041fee77175568e6c
손희찬 <heechanson1@gmail.com>
2024-01-15 20:34:18 +0900

    just
    z

--- Source/seiryu/JoyStick.cpp
+++ Source/seiryu/JoyStick.cpp
@@ -1,29 +1,28 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Blitzcrank.h"
+#include "JoyStick.h"
+#include "Kismet/KismetMathLibrary.h"
 
 // Sets default values
-ABlitzcrank::ABlitzcrank()
+AJoyStick::AJoyStick()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-	//RotateSpeed = 30.0f;
+
 }
 
 // Called when the game starts or when spawned
-void ABlitzcrank::BeginPlay()
+void AJoyStick::BeginPlay()
 {
 	Super::BeginPlay();
 	
 }
 
 // Called every frame
-void ABlitzcrank::Tick(float DeltaTime)
+void AJoyStick::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-	FVector vec = GetActorLocation() += FVector(1, 0, 0);
-	SetActorLocation(vec);
+	//FRotator Rotator = UKismetMathLibrary::FindLookAtRotation(GetActorLocation(), )
 }
 
- 
